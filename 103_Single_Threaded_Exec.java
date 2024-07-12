235dda22bf6163d1fdf3c4f9813b60bd101f699f
Meet-301 <pujarameet301@gmail.com>
2024-07-12 14:11:33 +0000

    Jul 12, 2024, 7:41 PM

--- 103_Single_Threaded_Exec.java
+++ 103_Single_Threaded_Exec.java
@@ -1,5 +1,10 @@
-class test {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class SingleThreadedEXecutor {
     public static void main(String args[]) {
-        System.out.println();
+        System.out.println("\nSingle Threaded Executor\n");
+
+        ExecutorService exec = Executors.newSingleThreadExecutor();
     }
 }
