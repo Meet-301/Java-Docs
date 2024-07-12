30a4876eca1298780bce73e8037d87608f98debc
Meet-301 <pujarameet301@gmail.com>
2024-07-12 14:26:17 +0000

    Jul 12, 2024, 7:56 PM

--- Program103/SingleThreadedExecutor.java
+++ Program103/SingleThreadedExecutor.java
@@ -3,12 +3,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 class SingleThreadedExecutor {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
         System.out.println("\nSingle Threaded Executor\n");
 
         ExecutorService exec = Executors.newSingleThreadExecutor();
         PrintTask task = new PrintTask();
 
         exec.submit(task);
+
+        exec.shutdown();
     }
 }
