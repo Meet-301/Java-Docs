70318fdc21a8dc10ce0cc7e3958ab9ef65e5177b
Meet-301 <pujarameet301@gmail.com>
2024-07-13 11:50:44 +0000

    Jul 13, 2024, 5:20 PM

--- Program104/ThreadMaker.java
+++ Program104/ThreadMaker.java
@@ -5,7 +5,7 @@ public class ThreadMaker implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep((long) Math.ceil(Math.random()));
+            Thread.sleep((long) Math.ceil(Math.random()*5));
             System.out.println("\n" + Thread.currentThread().getName());
         } catch (InterruptedException e) {
             System.out.println("\nException " + e + " occurred!!!");
