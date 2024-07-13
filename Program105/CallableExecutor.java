75e8a4dbb526ec9c89db2460b38c37c4a9d216f3
Meet-301 <pujarameet301@gmail.com>
2024-07-13 12:13:28 +0000

    Jul 13, 2024, 5:43 PM

--- Program105/CallableExecutor.java
+++ Program105/CallableExecutor.java
@@ -1,9 +1,13 @@
 package Program105;
 
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CallableExecutor {
     public static void main(String[] args) {
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
 
         int n;
         Scanner scan = new Scanner(System.in);
