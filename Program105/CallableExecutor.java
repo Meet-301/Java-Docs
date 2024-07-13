190985d0b537ccbdc05d1a54a4b937ac84ba27b7
Meet-301 <pujarameet301@gmail.com>
2024-07-13 12:23:34 +0000

    Jul 13, 2024, 5:53 PM

--- Program105/CallableExecutor.java
+++ Program105/CallableExecutor.java
@@ -1,13 +1,14 @@
 package Program105;
 
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CallableExecutor {
+    @SuppressWarnings({ "unchecked", "unused", "resource" })
     public static void main(String[] args) {
-
-        ExecutorService service = Executors.newSingleThreadExecutor();
 
         int n;
         Scanner scan = new Scanner(System.in);
@@ -16,5 +17,14 @@ public class CallableExecutor {
         n = scan.nextInt();
 
         CallableMaker factorial = new CallableMaker(n);
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        try {
+            Future<Long> result = service.submit((Callable<Long>) factorial);
+        } catch (Exception e) {
+            System.out.println("Exception " + e + " occurred!!!");
+        }
+
     }
 }
