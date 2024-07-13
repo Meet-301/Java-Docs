113d87ff6b3f2424b915fd951d5383ea179d0799
Meet-301 <pujarameet301@gmail.com>
2024-07-13 13:22:30 +0000

    Jul 13, 2024, 6:52 PM

--- Program104/ThreadExecutor.java
+++ Program104/ThreadExecutor.java
@@ -6,18 +6,21 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadExecutor {
     public static void main(String[] args) throws InterruptedException {
+
         ExecutorService exec = Executors.newFixedThreadPool(3);
 
         ThreadMaker task1 = new ThreadMaker();
         ThreadMaker task2 = new ThreadMaker();
         ThreadMaker task3 = new ThreadMaker();
 
-        exec.submit(task1);   
-        exec.submit(task2);   
+        exec.submit(task1);
+        exec.submit(task2);
         exec.submit(task3);
 
+        System.out.println("\n");
+
         exec.shutdown();
-        
+
         if (!exec.awaitTermination(10, TimeUnit.SECONDS)) {
             exec.shutdownNow();
         }
