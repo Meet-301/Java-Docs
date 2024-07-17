445e817659616aa911545fc87528dbbad8399f09
Meet-301 <pujarameet301@gmail.com>
2024-07-17 15:08:40 +0000

    Jul 17, 2024, 8:38 PM

--- 108_Filter_Usage.java
+++ 108_Filter_Usage.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
 class FilterUsage {
+    @SuppressWarnings("unused")
     public static void main(String args[]) {
         System.out.println("\nFilter Usage\n");
+
+        List<String> list = Arrays.asList("ten_length_string","notTen");
+        
+        
     }
 }
