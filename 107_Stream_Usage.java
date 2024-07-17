23a449d3ac232e5e5d9761ae58a0feb327b2882f
Meet-301 <pujarameet301@gmail.com>
2024-07-17 14:59:21 +0000

    Jul 17, 2024, 8:29 PM

--- 107_Stream_Usage.java
+++ 107_Stream_Usage.java
@@ -1,11 +1,11 @@
-// import java.util.Arrays;
-// import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
 class Streams {
     public static void main(String args[]) {
         System.out.println("\nStreams Usage\n");
 
-        // List<String> sArray = Arrays.asList("One","Two","Three");
-        // sArray.stream().forEach((String m) -> System.out.println(m));
+        List<String> sArray = Arrays.asList("One","Two","Three");
+        sArray.stream().forEach((String m) -> System.out.println(m));
     }
 }
