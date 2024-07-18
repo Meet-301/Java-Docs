c62a68b6dcc25dc60037aea552ac89de12fd5961
Meet-301 <pujarameet301@gmail.com>
2024-07-18 05:48:10 +0000

    Jul 18, 2024, 11:18 AM

--- 108_Filter_Usage.java
+++ 108_Filter_Usage.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class FilterUsage {
-    @SuppressWarnings("unused")
     public static void main(String args[]) {
         System.out.println("\nFilter Usage\n");
 
-        List<String> list = Arrays.asList("ten_length_string","notTen");
-        List<String> newList = new ArrayList<>();
+        List<String> list = Arrays.asList("ten_length_string","notTen","_notTen2");
+        StringBuilder sb = new StringBuilder();
         
         list.stream().filter(s -> s.length() > 10).forEach(s -> System.out.println(s));
+        list.stream().filter(s -> s.length() <= 10).forEach(s -> sb.append(s));
+        System.out.println("New Concatenated String is: "+sb);
     }
 }
