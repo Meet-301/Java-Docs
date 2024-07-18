b786dcd6a071538131638f2905e3f34c92473577
Meet-301 <pujarameet301@gmail.com>
2024-07-18 05:57:31 +0000

    Jul 18, 2024, 11:27 AM

--- 109_Odd_Filteration.java
+++ 109_Odd_Filteration.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 class OddFilteration {
     public static void main(String args[]) {
         System.out.println("\nOdd Filteration\n");
+
+        List<Integer> list = List.of(1,2,3,4,5);
+        list.stream().filter(s -> {
+            if (s % 2 != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 }
