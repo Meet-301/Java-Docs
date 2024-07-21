2f3fee2bd1212535a2830ed203bbf5ef11254787
Meet-301 <pujarameet301@gmail.com>
2024-07-21 09:45:18 +0000

    Jul 21, 2024, 3:15 PM

--- Program110/Tester.java
+++ Program110/Tester.java
@@ -1,5 +1,20 @@
 package Program110;
 
 public class Tester {
-    
+    Functional_Interface fi = new Functional_Interface() {
+        public boolean isPrime(int n) {
+            boolean prime = false;
+            for (int i = 2; i <= n; i++) {
+                if (n % 2 == 0) {
+                    prime = true;
+                }
+                else {
+                    return prime;
+                }
+            }
+            return prime;
+        };
+    };
+
+    boolean primeNum = fi.isPrime(0);
 }
