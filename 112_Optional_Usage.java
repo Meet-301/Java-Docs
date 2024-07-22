50aaf8fb7f193fa0ba74bc9e71bf251f3d92862b
Meet-301 <pujarameet301@gmail.com>
2024-07-22 11:45:45 +0000

    Jul 22, 2024, 5:15 PM

--- 112_Optional_Usage.java
+++ 112_Optional_Usage.java
@@ -1,7 +1,15 @@
+import java.util.Optional;
+
 class OptionalUsage {
+
+    Optional<String> setOptional(String str) {
+        Optional<String> empty = Optional.empty();
+        return empty;
+    }
+
     public static void main(String args[]) {
         System.out.println("\nOptional Class Usage\n");
 
-        
+
     }
 }
