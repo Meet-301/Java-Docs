c181ec04849684a1df5a6789d98279eafe23d5df
Meet-301 <pujarameet301@gmail.com>
2024-07-25 04:53:00 +0000

    Jul 25, 2024, 10:23 AM

--- 113_Intermediate_Oprs.java
+++ 113_Intermediate_Oprs.java
@@ -1,6 +1,10 @@
+import java.util.List;
+
 class IntermediateOperations {
     public static void main(String args[]) {
         System.out.println("\nIntermediate Operations\n");
+
+        List<Integer> list = List.of(1,2,3);
 
         
     }
