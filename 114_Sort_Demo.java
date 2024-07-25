e410bf8273b90d868332642932cb6b6947a242e9
Meet-301 <pujarameet301@gmail.com>
2024-07-25 11:19:14 +0000

    Jul 25, 2024, 4:49 PM

--- 114_Sort_Demo.java
+++ 114_Sort_Demo.java
@@ -1,7 +1,19 @@
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 class SortMethodDemo {
+
+     class sortBySalary implements Comparator<SortMethodDemo>  {
+
+        @Override
+        public int compare(SortMethodDemo o1, SortMethodDemo o2) {
+           return 0;
+        }
+    
+        
+    }
+
     public static void main(String args[]) {
         System.out.println("\nSort Method Demo\n");
 
