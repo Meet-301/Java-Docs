8bf682d70edb0eb7310b355b39910b1b44977907
Meet-301 <pujarameet301@gmail.com>
2024-07-25 12:01:15 +0000

    Jul 25, 2024, 5:31 PM

--- Program114/TestingEmployee.java
+++ Program114/TestingEmployee.java
@@ -7,7 +7,7 @@ public class TestingEmployee {
         System.out.println("\nEmployee Testing\n");
 
         List<Employee> employees = List.of(new Employee("one", 78000),
-        new Employee("Viraj", 87000),new Employee("K.C", 810000));
+        new Employee("two", 87000),new Employee("three", 810000));
 
         employees.stream().
         sorted((emp1,emp2) -> Integer.compare(emp1.getSalary(), emp2.getSalary()))
