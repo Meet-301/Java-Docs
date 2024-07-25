a5cfc7b5d1eeb5e1fa2a627855fc64ab637707a2
Meet-301 <pujarameet301@gmail.com>
2024-07-25 12:54:42 +0000

    Jul 25, 2024, 6:24 PM

--- 115_Map_Operations_Demo.java
+++ 115_Map_Operations_Demo.java
@@ -6,8 +6,6 @@ class MapOperationDemo {
 
         List<String> numbers = List.of("1","2","3");
 
-        numbers.stream().forEach(Integer::parseInt);
-
-        numbers.stream().map(null);
+        numbers.stream().map(Integer::parseInt);
     }
 }
