d3374be7f9a7ed6502e00b814b9d37b44b7a3140
Meet-301 <pujarameet301@gmail.com>
2024-07-25 11:42:05 +0000

    Jul 25, 2024, 5:12 PM

--- Program114/Employee.java
+++ Program114/Employee.java
@@ -2,8 +2,24 @@ package Program114;
 
 class Employee {
 
+    private final String name;
+    private final int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
     public static void main(String[] args) {
-        String name;
+        
     }
     
 }
