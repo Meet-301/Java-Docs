c2e627a477075def028f4bcb0e3bd8d4f2f15ef1
Meet-301 <pujarameet301@gmail.com>
2024-07-25 13:01:57 +0000

    Jul 25, 2024, 6:31 PM

--- 115_Map_Operations_Demo.java
+++ 115_Map_Operations_Demo.java
@@ -4,10 +4,8 @@ class MapOperationDemo {
     public static void main(String args[]) {
         System.out.println("\nMap Operation Demo\n");
 
-        List<String> numbers = List.of("1","2","3");
+        List<String> numbers = List.of("1", "2", "3");
 
-        numbers.stream().map(Integer::parseInt).
-        map(num -> Math.pow(num, 2)).
-        reduce(null);
+        numbers.stream().map(Integer::parseInt).map(num -> Math.pow(num, 2)).reduce((a, b) -> a + b);
     }
 }
