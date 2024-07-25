5b1974b6d0711beaf6a9c22a36735405fcae4c03
Meet-301 <pujarameet301@gmail.com>
2024-07-25 04:49:45 +0000

    Jul 25, 2024, 10:19 AM

--- 113_Intermediate_Oprs.java
+++ 113_Intermediate_Oprs.java
@@ -2,6 +2,8 @@ class IntermediateOperations {
     public static void main(String args[]) {
         System.out.println("\nIntermediate Operations\n");
 
-        int a[] = {};
+        int a[] = {1,2,3};
+
+        
     }
 }
