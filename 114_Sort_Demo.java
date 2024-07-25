9a024934c17843b429d81ec8b2aa93eaa2366f24
Meet-301 <pujarameet301@gmail.com>
2024-07-25 11:11:17 +0000

    Jul 25, 2024, 4:41 PM

---
+++ 114_Sort_Demo.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class SortMethodDemo {
+    public static void main(String args[]) {
+        System.out.println("\nSort Method Demo\n");
+
+        Map<String,Integer> map = new HashMap<>();
+    }
+}
