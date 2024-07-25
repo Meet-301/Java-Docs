7a9c4ce9098ae646bb4ae55ce440167217dd9aca
Meet-301 <pujarameet301@gmail.com>
2024-07-25 11:45:50 +0000

    Jul 25, 2024, 5:15 PM

--- Program114/TestingEmployee.java
+++ Program114/TestingEmployee.java
@@ -1,5 +1,11 @@
 package Program114;
 
+import java.util.List;
+
 public class TestingEmployee {
-    
+    public static void main(String[] args) {
+        System.out.println("\nEmployee Testing\n");
+
+        List<Employee> employees = List.of(new Employee("Meet", 0));
+    }
 }
