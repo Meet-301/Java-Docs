a5d8ff3abac3021e6ee63b341eafcc4eb7992cc0
Meet-301 <pujarameet301@gmail.com>
2024-07-27 09:16:39 +0000

    Jul 27, 2024, 2:46 PM

--- Ex8.java
+++ Ex8.java
@@ -7,7 +7,7 @@ class Ex8 {
 
         for (int i = 0; i < rows; i++) {
 
-            for (int j = 0; j < rows - i - 1; j++) {
+            for (int j = 0; j < rows - i; j++) {
                 System.out.print(" ");
             }
 
