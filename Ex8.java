7ef524cd40f54f5bf9f235828439e330f347ac95
Meet-301 <pujarameet301@gmail.com>
2024-07-27 08:55:44 +0000

    Jul 27, 2024, 2:25 PM

--- Ex8.java
+++ Ex8.java
@@ -6,10 +6,21 @@ class Ex8 {
         int size = VariableScanner.scanInt(true);
 
         int n[] = new int[size];
+        int odd = 0, even = 0;
 
-        System.out.println("Enter "+size+" elements");
+        System.out.println("Enter " + size + " elements");
         for (int i = 0; i < n.length; i++) {
             n[i] = VariableScanner.scanInt(true);
         }
+
+        for (int i = 0; i < n.length; i++) {
+            if (n[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.printf("\nOdd Numbers: %d, Even Numebers: %d", odd, even);
     }
 }
