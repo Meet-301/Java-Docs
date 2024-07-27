3ec94b45ec1687cb79a68ceffc6fef6e150c3f43
Meet-301 <pujarameet301@gmail.com>
2024-07-27 08:50:55 +0000

    Jul 27, 2024, 2:20 PM

---
+++ Ex8.java
@@ -0,0 +1,10 @@
+class Ex8 {
+    public static void main(String args[]) {
+        System.out.println("\nExercise 8\n");
+
+        System.out.print("Enter array size: ");
+        int size = VariableScanner.scanInt(true);
+
+        
+    }
+}
